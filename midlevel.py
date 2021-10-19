3fe1418177b32d90dc30b59acb909efc69913e39
Nicola0824 <yongtaow@kth.se>
2021-10-19 10:57:51 +0200

    midlevel.py finished

--- midlevel.py
+++ midlevel.py
@@ -1,41 +1,18 @@
-from PIL import Image
-import torchvision.transforms.functional as TF
+import pdb
 import visualpriors
-import subprocess
+import torch
+from config import DEVICE
 
-def mid_level_representation(input_image_tensor, representation_name):
+
+
+def mid_level_representations(input_image_tensor, representation_names):
     """
-    :return: pytorch tensor for 1 representation
+    :param input_image_tensor:  (batch_size, 3, 256, 256)
+    :param representation_names: list
+    :return: concatted image tensor to pass into FCN  (batch_size, 8*len(representation_names), 256, 256)
     """
-    pass
-
-def concat_representation(input_image_tensor, representation_names):
-    """
-    :param input_image_tensor:
-    :param representation_names:
-    :return: concatted image tensor to pass into FCN
-    """
-    pass
-
-
-
-
-if __name__ == '__main__':
-    # # Download a test image
-    # subprocess.call("curl -O https://raw.githubusercontent.com/StanfordVL/taskonomy/master/taskbank/assets/test.png", shell=True)
-
-    # Load image and rescale/resize to [-1,1] and 3x256x256
-    image = Image.open('test.png')
-    x = TF.to_tensor(TF.resize(image, 256)) * 2 - 1
-    x = x.unsqueeze_(0)
-
-    feature = 'keypoints3d'
-    device = 'cpu'
-    # Transform to normals feature
-    representation = visualpriors.representation_transform(x, feature, device=device)
-
-    # Transform to normals feature and then visualize the readout
-    pred = visualpriors.feature_readout(x, feature, device=device)
-
-    # Save it
-    TF.to_pil_image(pred[0] / 2. + 0.5).save(f'test_{feature}_readout.png')
+    representations=[]
+    for name in representation_names:
+        # (batch_size, 3, 256, 256) ——>(batch_size, 8, 16, 16)
+        representations.append(visualpriors.representation_transform(input_image_tensor, name, device=DEVICE))
+    return torch.cat(representations,dim=1)

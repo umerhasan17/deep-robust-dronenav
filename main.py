3a0be8c9a2800e08a81f6bb13a9baabc71559e48
Titouan Renard <titouan.renard@gmail.com>
2021-10-28 11:10:37 +0200

    cleaner pipeline and comment schematic

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
+# =============== Processing graph =====================
+#
+#                      image
+#                        |
+#                        | variable name: img
+#                        v
+#           ---------------------------
+#           |mid_level_representations|
+#           ---------------------------
+#                        |
+#                        | variable name: mid_level
+#                        v
+#                 ---------------
+#                 |   decoder   |
+#                 ---------------
+#                        |
+#                        | variable name: map_update
+#                        v
+#                   -------------                   -----------------
+#  TODO: Implement  |  combine  |   <-------------  |   transform   | <------------- previous_map
+#                   -------------                   -----------------
+#                        |
+#                        | variable name: map
+#                        v
+#                 ---------------
+#  TODO: Implement|   policy    |
+#                 ---------------
+#                        |
+#                        |
+#                        v
+#                   velocities
+#
+
+
 import pdb
 
 from PIL import Image
@@ -14,26 +48,26 @@ if __name__ == '__main__':
     print("download image to debug...")
     subprocess.call("curl -O https://raw.githubusercontent.com/StanfordVL/taskonomy/master/taskbank/assets/test.png", shell=True)
     image = Image.open('test.png')
-    x = TF.to_tensor(TF.resize(image, 256)) * 2 - 1
-    x = x.unsqueeze(0) #(1,3,256,256)
-    x = x.repeat(BATCHSIZE, 1, 1, 1)
+    img = TF.to_tensor(TF.resize(image, 256)) * 2 - 1
+    img = img.unsqueeze(0) #(1,3,256,256)
+    img = img.repeat(BATCHSIZE, 1, 1, 1)
 
 
     #==========Mid level encoder==========
     print("Passing mid level encoder...")
-    x=mid_level_representations(x,REPRESENTATION_NAMES)
+    mid_level=mid_level_representations(img,REPRESENTATION_NAMES)
 
     # ==========FC==========
     print("Passing fully connected layer...")
     fc=FC()
-    x = x.view(BATCHSIZE,-1) #x = torch.flatten(x, start_dim=1)  # flatten all dimensions except batc
-    x = fc(x)
-    x = x.view(BATCHSIZE,8*len(REPRESENTATION_NAMES),16,16)
+    mid_level = mid_level.view(BATCHSIZE,-1) #x = torch.flatten(x, start_dim=1)  # flatten all dimensions except batc
+    mid_level = fc(mid_level)
+    mid_level = mid_level.view(BATCHSIZE,8*len(REPRESENTATION_NAMES),16,16)
 
     # ==========Deconv==========
     print("Passing residual decoder...")
     decoder = ResNet(layers=RESIDUAL_LAYERS_PER_BLOCK,channels=RESIDUAL_NEURON_LISTS,strides=STRIDES).to(DEVICE)
-    x = decoder(x)
+    map_update = decoder(mid_level)
 
     print("Done!")
 

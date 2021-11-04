00f09a7a46f9f82bec4147190af7ab1402532090
Umer Hasan <umerhasan17@gmail.com>
2021-11-04 03:21:39 +0100

    Updates to main file in repo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """ 
 main.py
 ----------------------------------------------------------------------------
-     Authors : Yongtao Wu, Umer Hasan Sayed, Titouan Renard
+     Authors : Yongtao Wu, Umer Hasan, Titouan Renard
      Last Update : Octobre 2021
 
      Main routine for our deep cognitive drone mapping agent,
@@ -29,13 +29,13 @@ Processing graph:
           |                           |  (BATCHSIZE x 2048*REPRESENTATION_NUMBER) -(fc)->  (BATCHSIZE x 2*REPRESENTATION_NUMBER x 16 x 16) 
           -----------------------------  
                        |
-                       | variable name: map_update  -- (BATCHSIZE x 2 x 256 x 256) tensor // encodes confidence and free space channels
+                       | variable name: map_update  -- (BATCHSIZE x 2 x M x M) tensor // encodes confidence and free space channels
                        |
                        |                                  egomotion
                        |                                  | variable name : dx -- (3x1) numpy array, (x,y,theta)
                        v                                  v
                   -----todo----                   -------done------
- TODO: Implement  |  combine  |   <-------------  |   transform   | <------------- previous_map -- (BATCHSIZE x 2 x 256 x 256) tensor
+ TODO: Implement  |  combine  |   <-------------  |   transform   | <------------- previous_map -- (BATCHSIZE x 2 x M x M) tensor
                   -------------                   -----------------
                        |
                        | variable name: map 
@@ -69,7 +69,7 @@ from config import REPRESENTATION_NAMES,BATCHSIZE,DEVICE,RESIDUAL_LAYERS_PER_BLO
 
 def forward(image,egomotion,prev_map,verbose = False):
      #==========download image to debug==========
-    
+
     img = TF.to_tensor(TF.resize(image, 256)) * 2 - 1
     img = img.unsqueeze(0)                                                 # (1,3,256,256)
     activation = img.repeat(BATCHSIZE, 1, 1, 1)                            # (BATCHSIZE x 3 x 256 x 256) tensor
@@ -82,7 +82,7 @@ def forward(image,egomotion,prev_map,verbose = False):
     # ==========FC==========
     print("Passing fully connected layer...")
     fc=FC()
-    activation = activation.view(BATCHSIZE,-1)                                  # flatten all dimensions except batch, 
+    activation = activation.view(BATCHSIZE,-1)                                  # flatten all dimensions except batch,
                                                                                 # --> tensor of the form (BATCHSIZE x 2048*REPRESENTATION_NUMBER)
     activation = fc(activation)                                                 # pass through dense layer --> (BATCHSIZE x 2048*REPRESENTATION_NUMBER) tensor
     activation = activation.view(BATCHSIZE,8*len(REPRESENTATION_NAMES),16,16)   # after fully connected layer, # (BATCHSIZE x REPRESENTATION_NUMBER*2 x 16 x 16) tensor
@@ -104,7 +104,7 @@ if __name__ == '__main__':
      print("download image to debug...")
      subprocess.call("curl -O https://raw.githubusercontent.com/StanfordVL/taskonomy/master/taskbank/assets/test.png", shell=True)
      image = Image.open('test.png') #example image valuec
-     prev_map_raw = Image.open('Bedroom.jpg') #example prevmap value
+     # prev_map_raw = Image.open('Bedroom.jpg') #example prevmap value
      prev_map = TF.to_tensor(TF.resize(image, 256))[0:2] * 2 - 1
      prev_map = prev_map.unsqueeze(0)                                                 # (1,3,256,256)
      prev_map = prev_map.repeat(BATCHSIZE, 1, 1, 1)                            # (BATCHSIZE x 3 x 256 x 256) tensor
@@ -117,7 +117,7 @@ if __name__ == '__main__':
 
      # Starting environments
      from env import make_vec_envs
-     from utils import get_args
+     from utils.arguments import get_args
      torch.set_num_threads(1)
      args = get_args()
      envs = make_vec_envs(args)

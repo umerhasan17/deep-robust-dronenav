59922ed529c1cb4af4a03d1d4195deeee0897fb3
Umer Hasan <umerhasan17@gmail.com>
2021-11-04 03:18:49 +0100

    Add habitat env code

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import torch
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+DEVICE = 'cpu'
 
 REPRESENTATION_NAMES = ['keypoints3d','depth_euclidean']
 
@@ -8,9 +9,12 @@ RESIDUAL_LAYERS_PER_BLOCK = [2,2,2,2]
 RESIDUAL_SIZE   = [32, 64, 128, 256]
 RESIDUAL_NEURON_CHANNEL   = [16, 8, 4, 2, 2]
 STRIDES = [1, 1, 1]
-
+IMG_DIMENSIONS = (3, 256, 256) # mid level reps are in colour right now
+# MAP_DIMENSIONS =
 BATCHSIZE = 4
 
 MAP_SIZE = 5 # map size (in [m]), given a 256x256 map, picking map size = 5 gives a resolution of ~2cm
 
 
+# TODO add habitat_config yaml path
+# TODO add data path for use in habitat_config

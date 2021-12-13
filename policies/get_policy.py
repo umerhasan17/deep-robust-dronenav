5fe6151fea35d000470720f09d4be3f4346054e9
umerhasan17 <umerhasan17@gmail.com>
2021-12-13 18:46:17 +0100

    Update key in get policy object

--- policies/get_policy.py
+++ policies/get_policy.py
@@ -7,7 +7,7 @@ from policies.actual_map_policy import PointNavDRRNActualMapPolicy
 def get_current_policy_object(policy_name):
     policy_objects = dict(
         Baseline=PointNavBaselinePolicy,
-        BaselineMidlevel=PointNavBaselineMidLevelPolicy,
+        BaselineMidLevel=PointNavBaselineMidLevelPolicy,
         DRRN=PointNavDRRNPolicy,
         DRRNActualMap=PointNavDRRNActualMapPolicy,
     )
